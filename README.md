a0ba1b8c0397f851b3dc42ffe18d1ee5f7789831
Atsushi Mori <ki4070ma@gmail.com>
2019-04-01 00:06:21 +0900

    Added Label Encoder and filled NaN

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 https://qiita.com/katsu1110/items/a1c3185fec39e5629bcb
 
 # Dependencies
-* ```pip install numpy pandas seaborn matplotlib```
+* ```pip install numpy pandas seaborn matplotlib sklearn missingno```
--- main.py
+++ main.py
@@ -10,3 +10,39 @@ train = pd.read_csv('./input/train.csv')
 test = pd.read_csv('./input/test.csv')
 
 print(train.dtypes)
+
+print(train['SaleCondition'].unique())
+
+# ****
+
+# string label to categorical values
+from sklearn.preprocessing import LabelEncoder
+
+for i in range(train.shape[1]):
+    if train.iloc[:,i].dtypes == object:
+        lbl = LabelEncoder()
+        lbl.fit(list(train.iloc[:,i].values) + list(test.iloc[:,i].values))
+        train.iloc[:,i] = lbl.transform(list(train.iloc[:,i].values))
+        test.iloc[:,i] = lbl.transform(list(test.iloc[:,i].values))
+
+print(train['SaleCondition'].unique())
+
+# ****
+
+# search for missing data
+import missingno as msno
+msno.matrix(df=train, figsize=(20,14), color=(0.5,0,0))
+
+# keep ID for submission
+train_ID = train['Id']
+test_ID = test['Id']
+
+# split data for training
+y_train = train['SalePrice']
+X_train = train.drop(['Id','SalePrice'], axis=1)
+X_test = test.drop('Id', axis=1)
+
+# dealing with missing data
+Xmat = pd.concat([X_train, X_test])
+Xmat = Xmat.drop(['LotFrontage','MasVnrArea','GarageYrBlt'], axis=1)
+Xmat = Xmat.fillna(Xmat.median())
